db0ba862ed0c9c645236d7e197f34e34e6630f87
Priwinn <43821596+Priwinn@users.noreply.github.com>
2019-10-10 22:53:17 +0200

    Update accenture.py

--- accenture.py
+++ accenture.py
@@ -36,14 +36,3 @@ cyl(3453,5)
 print("--- %s seconds ---" % (time.time() - start_time))
 
 
-
-primes=[1,2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-
-for i in range(1000):
-  for prime in primes:
-    a=cyl(i,prime)[1]
-    if a==4:
-      print(i)
-      print(prime)
-      break
-
